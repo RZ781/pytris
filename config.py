0abe824e28733a75010d92e0b6a533e36e48d1d4
R Z <91019650+RZ781@users.noreply.github.com>
2025-05-23 23:27:51 +0100

    Stopped crashing when config files are corrupted

--- config.py
+++ config.py
@@ -9,8 +9,11 @@ def load(name: str) -> Any:
     path = f"{pytris_folder}/{name}.json"
     if not os.path.isfile(path):
         return None
-    with open(path) as f:
-        return json.load(f)
+    try:
+        with open(path) as f:
+            return json.load(f)
+    except Exception:
+        return None
 
 def save(name: str, data: Any) -> None:
     if not os.path.isdir(pytris_folder):
